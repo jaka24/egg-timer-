1a3bb4977ef66b0cf2670ef8a0754c7de3ffc209
John-marc Attis <attisjohn@yahoo.com>
2021-12-19 18:19:56 -0500

    class exit

--- egg timer/ViewController.swift
+++ egg timer/ViewController.swift
@@ -10,51 +10,50 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    @IBOutlet weak var outputlabel: UILabel!
+    @IBOutlet weak var outputLabel: UILabel!
     
-    @IBOutlet weak var progressview: UIProgressView!
+    @IBOutlet weak var progressView: UIProgressView!
     
     
     
-var eggTimes = ["Hard" : 10, "Medium" : 7, "Soft" : 4] //Dictionary
+var eggTime = ["Hard" : 10, "Medium" : 7, "Soft" : 4] //Dictionary
 
-        var starttime = 0
-        var totaltime = 0
+        var startTime = 0
+        var totalTime = 0
 
     
     var timer = Timer()
 
-        @IBAction func clickme(_ sender: UIButton) {
+    
+    
+    @IBAction func clickMe(_ sender: UIButton) {
+        timer.invalidate()
+        
+        progressView.progress = 0.0
+        startTime = 0
+        
        
-            
-            timer.invalidate()
-            progressView.progress = 0.0
-            startTime = 0
+        let hardness = sender.currentTitle!
         
-            let hardness = sender.currentTitle!
-            print(hardness)
-            
-totaltime = eggTimes[hardness]! //10 7 4
+        
+        print(hardness)
+        
+        totalTime = eggTime[hardness]!
+        
+        
         Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
-
     }
-
-
-@objc func updateCounter() {
-   
-        if starttime < totalTime  {
+    
+    @objc func updateCounter() {
+        if startTime < totalTime {
             
-           
-            starttime += 1
-            progressView.progress= Float(startTime)/Float(totalTime)
-            
-        
-            else do {
-            
-            outputLabel.text = "Eggs are ready!"
-           .self.timer.invalidate()
+            startTime += 1
+            progressView.progress = Float(startTime)/(Float(totalTime))
         }
-            }
-
-}}}
+        else{
+            outputLabel.text = "Eggs are ready!"
+            timer.invalidate()
+        }
+    }
 }
+
